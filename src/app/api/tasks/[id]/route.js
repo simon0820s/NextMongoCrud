876eb9f5cc512ca0c7b97fa89ea2f567c170f4fa
simon0820s <simon0820s@gmail.com>
2023-08-21 09:38:43 -0500

    create DELETE and PUT function

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,8 +1,19 @@
 import { NextResponse } from "next/server";
 
-export function GET(request, {params}) {
-  
+export function GET(request, { params }) {
   return NextResponse.json({
-    message: params
+    message: params,
+  })
+}
+
+export function DELETE(request, { params }) {
+  return NextResponse.json({
+    message: `deleting task ${params.id} ...`,
+  })
+}
+
+export function PUT(request, { params }) {
+  return NextResponse.json({
+    message: `updating task ${params.id} ...`,
   })
 }

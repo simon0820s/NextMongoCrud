22049775af7e5642442ba6ae1c5048ddc4b71df2
simon0820s <simon0820s@gmail.com>
2023-08-21 22:57:03 -0500

    creating tasks

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -3,19 +3,23 @@ import { connectDB } from "@/utils/mongoose";
 import Task from "@/models/Task";
 
 export async function GET() {
-  connectDB()
+  connectDB();
 
-  const tasks = await Task.find()
+  const tasks = await Task.find();
 
-  return NextResponse.json(tasks)
+  return NextResponse.json(tasks);
 }
 
 export async function POST(request) {
-  
-  const data = await request.json()
+  const data = await request.json();
 
+  const newTask = new Task(data);
+
+  const savedTask = await newTask.save();
+
+  console.log(savedTask);
 
   return NextResponse.json({
-    message: data
-  })
+    message: "task created",
+  });
 }

dcfba6124bff0e55f8c15b8f736e926219cac915
simon0820s <simon0820s@gmail.com>
2023-08-22 18:46:29 -0500

    get task by _id of query params

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,19 +1,39 @@
 import { NextResponse } from "next/server";
+import { connectDB } from "@/utils/mongoose";
+import Task from "@/models/Task";
 
-export function GET(request, { params }) {
-  return NextResponse.json({
-    message: params,
-  })
+export async function GET(request, { params }) {
+  try {
+    connectDB();
+    const taskFound = await Task.findById(params.id);
+
+    if (!taskFound) {
+      return NextResponse.json({
+        message: "task nof found",
+      },{
+        status: 404
+      });
+    }
+
+    return NextResponse.json({
+      message: taskFound
+    });
+
+  } catch (error) {
+    return NextResponse.json(error.message, {
+      status: 400,
+    });
+  }
 }
 
 export function DELETE(request, { params }) {
   return NextResponse.json({
     message: `deleting task ${params.id} ...`,
-  })
+  });
 }
 
 export function PUT(request, { params }) {
   return NextResponse.json({
     message: `updating task ${params.id} ...`,
-  })
+  });
 }

17b6831de16d771518979de80eb7f57465bcd134
simon0820s <simon0820s@gmail.com>
2023-08-28 19:30:28 -0500

    style inputs

--- src/app/tasks/new/page.jsx
+++ src/app/tasks/new/page.jsx
@@ -1,9 +1,12 @@
 function FormPage() {
   return (
-    <div>
+    <div className="h-[calc(100vh-7rem)] flex justify-center items-center">
       <form>
-        <input type="text" name="title" placeholder="Title" />
-        <textarea name="description" placeholder="Description" ></textarea>
+        <input type="text" name="title" placeholder="Title"
+          className="bg-zinc-800 border-2 border-zinc-700 w-full p-4 rounded-lg my-4" />
+        <textarea name="description" placeholder="Description"
+          className="bg-zinc-800 border-2 border-zinc-700 w-full p-4 rounded-lg my-4"
+        ></textarea>
         <button>
           Save
         </button>

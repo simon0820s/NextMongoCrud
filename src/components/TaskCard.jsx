e947d326b75d987726898e6dd59fbb28ac6f0504
simon0820s <simon0820s@gmail.com>
2023-09-05 19:41:15 -0500

    redirect to task/

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,8 +1,12 @@
+import Link from "next/link"
+
 export default function TaskCard({ task }) {
   return (
-    <article className="bg-zinc-700 p-4 rounded-md hover:cursor-pointer hover:bg-zinc-800 transition-all ease-in-out duration-200">
-      <h3>{task.title}</h3>
-      <p>{task.description}</p>
-    </article>
+    <Link href={`/tasks/${task._id}`}>
+      <article className="bg-zinc-700 p-4 rounded-md hover:cursor-pointer hover:bg-zinc-800 transition-all ease-in-out duration-200">
+        <h3>{task.title}</h3>
+        <p>{task.description}</p>
+      </article>
+    </Link>
   )
 }
